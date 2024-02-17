54f176eab80805f90274e4439138265240204c57
Devanshsati <thedevanshsati@gmail.com>
2024-02-17 23:25:48 +0530

    sidebar added

--- app/src/main/java/com/example/navigationgame/GameWonFragment.kt
+++ app/src/main/java/com/example/navigationgame/GameWonFragment.kt
@@ -1,9 +1,13 @@
 package com.example.navigationgame
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -17,8 +21,56 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view:View ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment3)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment3())
         }
+//        Toast.makeText(context,"Correct: ${args.numCorrect} , Total Question: ${args.numQuestions}",Toast.LENGTH_LONG).show()
         return binding.root
     }
+
+    private fun getShareIntent(): Intent{
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text,args.numCorrect,args.numQuestions))
+        return ShareCompat.IntentBuilder.from(requireActivity()).setText(getString(R.string.share_success_text,args.numCorrect,args.numQuestions)).setType("text/plain").intent
+    }
+
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        if(null==getShareIntent().resolveActivity(requireActivity().packageManager)){
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+        menu.apply { R.menu.winner_menu }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onContextItemSelected(item)
+    }
+
+//    @Deprecated("Deprecated in Java")
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        inflater.inflate(R.menu.winner_menu,menu)
+//        if(null==getShareIntent().resolveActivity(requireActivity().packageManager)){
+//          menu.findItem(R.id.share)?.isVisible = false
+//        }
+//    }
+//    @Deprecated("Deprecated in Java")
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId){
+//            R.id.share -> shareSuccess()
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
 }
--- app/src/main/java/com/example/navigationgame/TitleFragment.kt
+++ app/src/main/java/com/example/navigationgame/TitleFragment.kt
@@ -1,10 +1,13 @@
 package com.example.navigationgame
 
 import android.os.Bundle
-import android.view.*
-import androidx.fragment.app.Fragment
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.navigationgame.databinding.FragmentTitleBinding
@@ -16,7 +19,9 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        binding.playButton.setOnClickListener { v:View -> v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment()) }
+        //        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+
         return binding.root
     }
 
